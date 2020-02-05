148248e6603a20f191e2cee4513e211c936f7eb3
meggilbert15 <59583008+meggilbert15@users.noreply.github.com>
2020-02-04 20:39:00 -0800

    Styling 1
    Update favicons, add extra visuals

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,20 @@ const player = new Plyr('#player', {
     keyboard : { focused: true, global: true },
     seekTime : 5,
     invertTime : false,
+    youtube: { noCookie: false, rel: 0, showinfo: 0, iv_load_policy: 0, modestbranding: 0 },
+    
    });
-   
+
+
+///////////////////         BLINK           //////////////
+
+   window.addEventListener("load", function() {
+    var f = document.getElementById('title');
+    setInterval(function() {
+        f.style.display = (f.style.display == 'none' ? '' : 'none');
+    }, 800);
+}, false); 
+
+/////////////////////////////////////////////
+
+
